ca172a320fd0d682573cd1ec4d3373a240691e34
Eric O'Callaghan <eric@ericoc.com>
2024-12-14 22:39:07 -0500

    index links to all three with breadcrumbs

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,6 +3,6 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
     """Index page."""
-    extra_context = {"TITLE": "Utility"}
+    extra_context = {"TITLE": "Utilities"}
     http_method_names = ("get",)
     template_name = "index.html"
