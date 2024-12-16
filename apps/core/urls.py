e5a44f6a17ec59e340baefa0b05c72d00eb6b3f2
Eric O'Callaghan <eric@ericoc.com>
2024-12-15 19:16:38 -0500

    move urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 from .views import IndexView
 
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index")
+    path("", IndexView.as_view(), name="index"),
+    path("admin/", admin.site.urls, name="admin"),
+    path("i18n/", include('django.conf.urls.i18n')),
 ]
--- urls.py
+++ urls.py
@@ -1,18 +1,13 @@
 """isharmud.com root URL configuration."""
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
 from api import api_router
 
-
 urlpatterns = [
     path("", include("apps.core.urls"), name="core"),
-    path("admin/", admin.site.urls, name="admin"),
     path("api/", include(api_router.urls), name="api"),
-    path("i18n/", include('django.conf.urls.i18n')),
-
     path("electric/", include("apps.electric.urls"), name="electric"),
     path("gas/", include("apps.gas.urls"), name="gas"),
     path("water/", include("apps.water.urls"), name="water"),
